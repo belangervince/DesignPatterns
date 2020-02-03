cd509761951338e40fc63f44896c5b2521f20301
belangervince <belangervince@outlook.com>
2020-02-03 11:24:44 -0500

    Iterator pattern : the benefits

--- src/com/codewithmosh/iterator/BrowseHistory.java
+++ src/com/codewithmosh/iterator/BrowseHistory.java
@@ -1,43 +1,38 @@
 package com.codewithmosh.iterator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BrowseHistory {
-    private List<String> urls = new ArrayList<>(); // should be another data structure
+    private String[] urls = new String[10]; // should be another data structure
+    private int count;
 
     public void push(String url) {
-        urls.add(url);
+        urls[count++] = url;
     }
 
     public String pop() {
-        var lastIndex = urls.size() - 1;
-        var lastUrl = urls.get(lastIndex);
-        urls.remove(lastUrl);
-        return lastUrl;
+        return urls[--count];
     }
 
     public Iterator createIterator() {
-        return new ListIterator(this);
+        return new ArrayIterator(this);
     }
 
-    public class ListIterator implements Iterator {
+    public class ArrayIterator implements Iterator {
 
         private BrowseHistory history;
         private int index;
 
-        public ListIterator(BrowseHistory history){
+        public ArrayIterator(BrowseHistory history){
             this.history = history;
         }
 
         @Override
         public boolean hasNext() {
-            return index < history.urls.size();
+            return index < history.urls.length;
         }
 
         @Override
         public String current() {
-            return history.urls.get(index);
+            return history.urls[index];
         }
 
         @Override
