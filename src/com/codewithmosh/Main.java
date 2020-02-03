e6c9970f2d41ec9f3ab299f11fd4b0045392d18e
belangervince <belangervince@outlook.com>
2020-02-03 16:52:38 -0500

    Command pattern : the problem

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -5,7 +5,8 @@ import com.codewithmosh.template.TransferMoneyTask;
 public class Main {
 
     public static void main(String[] args) {
-        var task = new TransferMoneyTask();
-        task.execute();
+        // button
+        // checkbox
+        // textbox
     }
 }
---
+++ src/com/codewithmosh/command/Button.java
@@ -0,0 +1,17 @@
+package com.codewithmosh.command;
+
+public class Button {
+    private String label;
+
+    public void click() {
+        // ...
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
